c380e22fb6957b06daa0f039079931d27b964f53
Cloudev <copubah@gmail.com>
2024-06-11 18:22:22 +0300

    var.sh

--- var.sh
+++ var.sh
@@ -1,11 +1,19 @@
-
+# this is a comment
 ## variables;a var is a container used to store data  or values
+#Variable_name=value- the syntax for a variable,the value can be changed and used for multiple times
 
-#!/bin/bash
-marks=90
-echo $marks
-## it should print the value 90
+## Rules for bash variables
+# 1.Prefix the var name with $ sign while reading or printing a var
+# 2.Leave the dollar sign while setting a var with any value
+# 3.A var name may be alphanumeric or with an underscore
+# 4.The name is case sensitive 
+# 5.Can be written either in upper or lower case
 
-## string;strings are sequences of characters
-## interger;handles arithmetic
-##array;used to store multiple values under a single variable
+
+## System-defined var-are predefined and created and maintained by the Linux OS,they are defined in capital letters
+
+## Samples
+echo $BASH
+echo $BASH_VERSION
+echo $HOME
+echo $PWD
